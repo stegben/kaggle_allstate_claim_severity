99d7f7d022bccecce2e69dd822b255b0e68ff816
cph <cph@titan.r444b.ee.ntu.edu.tw>
2016-10-17 11:08:35 +0800

    update

--- feature_generation.py
+++ feature_generation.py
@@ -1,7 +1,8 @@
 import sys
+import itertools
 
 import pandas as pd
-
+import joblib
 
 def extract_data(train_fname, test_fname, rand_state=1234):
     """
@@ -26,18 +27,33 @@ def extract_data(train_fname, test_fname, rand_state=1234):
     for col in columns:
         print('process data column {}'.format(col))
         if 'cat' in col:
-            dummy = pd.get_dummies(df[col], dummy_na=True, drop_first=True, prefix=col)
+            dummy = pd.get_dummies(df[col], drop_first=True, prefix=col)
             df = pd.concat([df, dummy], axis=1).drop(col, axis=1)
         if col == 'cont2':
-            print('cont2 has {} unique values'.format(len(df[col].value_counts())))
-            dummy = pd.get_dummies(df[col], dummy_na=True, drop_first=True, prefix=col)
+            print('{} has {} unique values'.format(col, len(df[col].value_counts())))
+            dummy = pd.get_dummies(df[col], drop_first=True, prefix=col)
             df = pd.concat([df, dummy], axis=1)
         if 'cont' in col:
             target = df[col]
             df[col] = (target - target.mean()) / target.std()
 
-    df_train = df[~df['is_test']].drop('id')
-    df_test = df[df['is_test']]
+    cont_col_combinations = itertools.combinations([col for col in columns if 'cont' in col], 2)
+    for col_combination in cont_col_combinations:
+        col1 = col_combination[0]
+        col2 = col_combination[1]
+        new_feat_1 = (df[col2] / df[col1])
+        new_feat_1 = new_feat_1.fillna(new_feat_1.mean())
+        new_feat_name_1 = col2 + '_divide_' + col1
+        df[new_feat_name_1] = new_feat_1
+
+        new_feat_2 = (df[col2] * df[col1])
+        new_feat_name_2 = col2 + '_multiply_' + col1
+        df[new_feat_name_2] = new_feat_2
+
+
+    df_train = df[~df['is_test']].drop('id', axis=1)
+    df_test = df[df['is_test']].drop('loss', axis=1)
+    return df_train, df_test
 
 
 def main():
@@ -47,9 +63,8 @@ def main():
 
     # df_train, df_valid, df_test = extract_data(train_fname, test_fname, 1234)
     df_train, df_test = extract_data(train_fname, test_fname, 1234)
+    joblib.dump({'train': df_train, 'test': df_test}, data_fname)
 
-    joblib.dump(data_fname, {'train': df_train, 'test': df_test})
-    import ipdb; ipdb.set_trace()
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -1,29 +1,87 @@
 import sys
 
+import pandas as pd
+
 import joblib
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Activation
-# from keras.optimizers import Nadam
+from keras.layers.advanced_activations import PReLU
+from keras.layers.core import Dropout
+from keras.layers.noise import GaussianNoise
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l1, l2
+from keras.optimizers import Nadam
+
+
+def reference_model(input_dim):
+    model = Sequential()
+    model.add(Dense(400, input_dim=input_dim, init='he_normal'))
+    model.add(PReLU())
+    model.add(Dropout(0.4))
+    model.add(Dense(200, init='he_normal'))
+    model.add(PReLU())
+    model.add(Dropout(0.2))
+    model.add(Dense(1, init='he_normal'))
+    model.compile(loss='mae', optimizer='adadelta')
+    return(model)
+
+
+def modified_reference_model(input_dim):
+    model = Sequential()
+    model.add(Dense(300, input_dim=input_dim, init='he_normal'))
+    model.add(Activation('tanh'))
+    model.add(Dropout(0.4))
+    model.add(Dense(200, init='he_normal'))
+    model.add(PReLU())
+    model.add(Dropout(0.4))
+    model.add(Dense(200, init='he_normal'))
+    model.add(PReLU())
+    model.add(Dropout(0.4))
+    model.add(Dense(1, init='he_normal'))
+    model.add(Activation('softplus'))
+    model.compile(loss='mae', optimizer='adadelta')
+    return(model)
+
+
+def my_model(input_dim):
+    model = Sequential([
+        Dense(1000, input_dim=input_dim, W_regularizer=l1(0.0002), init='he_normal'),
+        Activation('tanh'),
+        # PReLU(init='zero', weights=None),
+        Dropout(0.2),
+        Dense(400, init='he_normal', W_regularizer=l2(0.0002)),
+        PReLU(init='zero', weights=None),
+        Dropout(0.2),
+        Dense(200, init='he_normal', W_regularizer=l2(0.0002)),
+        PReLU(init='zero', weights=None),
+        Dropout(0.2),
+        Dense(100, init='he_normal', W_regularizer=l2(0.0002)),
+        PReLU(init='zero', weights=None),
+        Dense(1),
+        Activation('softplus'),
+        # Activation('relu')
+    ])
+    optimizer = Nadam(lr=0.0002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.00005)
+    model.compile(optimizer=optimizer, loss='mae')
+    return model
+
 
 def train_model(train_x, train_y):
     input_dim = train_x.shape[1]
+    print(input_dim)
+    model = modified_reference_model(input_dim)
+    # model = reference_model(input_dim)
+    # model = my_model(input_dim)
 
-    model = Sequential([
-        Dense(2048, input_dim=input_dim, init='he_normal'),
-        Activation('relu'),
-        Dense(1),
-        Activation('softplus'),
-    ])
-
-    model.compile(optimizer='rmsprop', loss='mae')
-    model.fit(train_x, train_y, nb_epoch=10, batch_size=32, validation_split=0.2)
+    model.fit(train_x, train_y, nb_epoch=50, batch_size=128, validation_split=0.1)
     return model
 
 
 def main():
     data_fname = sys.argv[1]
+    sub_fname = sys.argv[2]
 
     print('Read data...')
     data = joblib.load(data_fname)
@@ -38,7 +96,9 @@ def main():
 
     test_x = df_test.drop('id', axis=1).values
     test_id = df_test['id']
-    pred = model.predict_proba()
+    pred = model.predict(test_x)
+    pd.DataFrame({'id': test_id, 'loss': pred.flatten()}).to_csv(sub_fname, index=False)
+
 
 if __name__ == '__main__':
     main()
